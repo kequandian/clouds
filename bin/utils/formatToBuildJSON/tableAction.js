bce63dc67ca032c75b19b78a10a11fd87639ee4d
LLH <youyeke@163.com>
2020-09-10 15:51:32 +0800

    调整输出的 json 的 action 的格式

--- bin/utils/formatToBuildJSON/tableAction.js
+++ bin/utils/formatToBuildJSON/tableAction.js
@@ -1,6 +1,6 @@
 
 function tableAction(action, pageName) {
-  const { title, type, ...rest } = action;
+  const { title, type, options, ...rest } = action;
   const rst = {
     title,
     type,
@@ -10,7 +10,19 @@ function tableAction(action, pageName) {
     rst.options.path = `${pageName}/${pageName}-${type}`;
     rst.type = 'path';
   }
-  return rst
+  if (type.indexOf('export') > -1 || type.indexOf('import') > -1) {
+    rst.type = `${type}-excel`;
+  }
+
+  if (options && options.api) {
+    options.API = options.api;
+    delete options.api;
+  }
+
+  return {
+    ...rst,
+    options,
+  }
 }
 
 module.exports = tableAction;
